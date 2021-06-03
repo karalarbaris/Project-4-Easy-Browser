2f312bd1d44d3973b07f9ca3877d02b4f7062395
Baris <karalarbaris@gmail.com>
2021-06-03 10:16:30 +0200

    Initial Commit

---
+++ Project-4-Easy-Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-4-Easy-Browser
+//
+//  Created by Baris Karalar on 3.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
