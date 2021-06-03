f78328379ba53cdcb2b96fbcb76eb99191138aa0
Baris <karalarbaris@gmail.com>
2021-06-03 11:15:23 +0200

    Choosing a website: UIAlertController action sheets

--- Project-4-Easy-Browser/ViewController.swift
+++ Project-4-Easy-Browser/ViewController.swift
@@ -21,16 +21,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.hackingwithswift.com/")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        //For ipad
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
 
-
+        present(ac, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://" + actionTitle) else { return }
+        webView.load(URLRequest(url: url))        
+    }
 }
 
 extension ViewController: WKNavigationDelegate {
-
-    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
-
